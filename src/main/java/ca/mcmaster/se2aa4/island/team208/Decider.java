8600995b022bab2e08a11ed25de21cdf5a89c294
Andrew Habib <andrew9ca@gmail.com>
2024-02-29 21:55:57 -0500

    Maneuver Drone to Ground Cell then return to safety efficiently + Refactor + Radar

--- src/main/java/ca/mcmaster/se2aa4/island/team208/Decider.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team208/Decider.java
@@ -10,63 +10,66 @@ import java.util.*;
 public class Decider {
 
     private final Logger logger = LogManager.getLogger();
-
     private final List<Action> decisionQueue;
     private final List<Results> results;
-
     private Drone drone;
-    private RescueAreaMap map;
+    private Radar radar;
     private int currentStep = 0; //next step that needs to be executed
 
-    public Decider(Drone drone, RescueAreaMap map) {
+    public Decider(Drone drone, Radar radar) {
         this.results = new ArrayList<>();
         this.decisionQueue = new ArrayList<>();
         this.drone=drone;
-        this.map=map;
-
+        this.radar=radar;
+        this.currentStep=0;
         this.decisionQueue.add(Action.ECHO_FRONT);
     }
 
     public JSONObject getNextStep(){
         JSONObject step;
-        if (currentStep >= decisionQueue.size()) {
+        if (this.currentStep >= decisionQueue.size()) {
             this.setNextDecision();
         }
-        step=this.translateDecision(decisionQueue.get(currentStep));
-        currentStep++;
+        step=this.translateDecision(decisionQueue.get(this.currentStep));
+        this.currentStep++;
         logger.info("** Decision: {}", step.toString());
         return step;
     }
 
     public void addResult(Results result){
-        results.add(result);
+        this.results.add(result);
     }
 
     //function needs to eventually reach "STOP" and stay there
     //function may generate more than one step with each call
     private void setNextDecision(){
-        int flyAmount = -1;
-
-        if(currentStep>0) {
-            JSONObject lastResult = results.get(currentStep - 1).getResponse().getJSONObject("extras");
-            if (lastResult.get("found").equals("OUT_OF_RANGE")) {
-                flyAmount = lastResult.getInt("range");
-                logger.info("Fly range is "+flyAmount);
-                logger.info("Decisions: "+decisionQueue.toString());
+        this.decisionQueue.add(Action.SCAN);
+        if (this.currentStep - 1 == 0 || this.decisionQueue.get(this.currentStep - 1) == Action.FLY) {
+            this.decisionQueue.add(Action.ECHO_RIGHT);
+        } else if (this.decisionQueue.get(this.currentStep - 1) == Action.ECHO_RIGHT) {
+            if (this.radar.getFound().equals("GROUND")) {
+                this.decisionQueue.add(Action.TURN_RIGHT);
+            }
+            this.decisionQueue.add(Action.ECHO_FRONT);
+        } else if (this.decisionQueue.get(this.currentStep - 1) == Action.ECHO_FRONT){
+            if (this.radar.getRange() == 0) {
+                this.decisionQueue.add(Action.STOP);
+            } else {
+                this.decisionQueue.add(Action.FLY);
             }
         }
-        for(int i=0; i<flyAmount;i++){
-            this.decisionQueue.add(Action.FLY);
-            this.decisionQueue.add(Action.SCAN);
-        }
-        if(flyAmount==0){
-            this.decisionQueue.add(Action.SCAN);
-            this.decisionQueue.add(Action.STOP);
-        }
-        else {
-            this.decisionQueue.add(Action.ECHO_FRONT);
-        }
-
+//        int flyAmount = (currentStep > 0) ? this.radar.getMaxDistanceBeforeMIA() : -1;
+//
+//        for(int i=0; i<flyAmount;i++){
+//            this.decisionQueue.add(Action.FLY);
+//            this.decisionQueue.add(Action.SCAN);
+//        }
+//        if(flyAmount==0){
+//            this.decisionQueue.add(Action.SCAN);
+//            this.decisionQueue.add(Action.STOP);
+//        } else {
+//            this.decisionQueue.add(Action.ECHO_FRONT);
+//        }
 
         /*
         try{
@@ -93,7 +96,43 @@ public class Decider {
 
          */
     }
-    /*
+
+
+    //this is done when decision is about to be performed
+    public JSONObject translateDecision(Action action){
+        JSONObject step = new JSONObject();
+
+        step.put("action", action.toString());
+        switch(action){
+            case TURN_LEFT -> {
+                step.put("parameters", new JSONObject().put("direction",Direction.getLeft(drone.getDirection()).toString()));
+                //updating drone should be in acknowledgingResults
+                this.drone.setDirection(Direction.getLeft(drone.getDirection()));
+            }
+            case TURN_RIGHT -> {
+                step.put("parameters", new JSONObject().put("direction",Direction.getRight(drone.getDirection()).toString()));
+                this.drone.setDirection(Direction.getRight(drone.getDirection()));
+            }
+            case ECHO_LEFT -> {
+                step.put("parameters", new JSONObject().put("direction",Direction.getLeft(drone.getDirection()).toString()));
+            }
+            case ECHO_RIGHT -> {
+                step.put("parameters", new JSONObject().put("direction",Direction.getRight(drone.getDirection()).toString()));
+            }
+            case ECHO_FRONT -> {
+                step.put("parameters", new JSONObject().put("direction",drone.getDirection().toString()));
+            }
+
+            default->{
+
+            }
+        }
+        //logger.info("** Decision: {}", step.toString());
+        return step;
+    }
+
+
+/*
     public void decide(String curr_decision) {
 
         this.decision = curr_decision;
@@ -124,35 +163,6 @@ public class Decider {
     }
 
      */
-
-    //this is done when decision is about to be performed
-    public JSONObject translateDecision(Action action){
-        JSONObject step = new JSONObject();
-
-        step.put("action", action.toString());
-        switch(action){
-            case TURN_LEFT,ECHO_LEFT -> {
-                step.put("parameters", new JSONObject().put("direction",Direction.getLeft(drone.getDirection()).toString()));
-
-                //updating drone should be in acknowledgingResults
-                this.drone.setDirection(Direction.getLeft(drone.getDirection()));
-            }
-            case TURN_RIGHT,ECHO_RIGHT -> {
-                step.put("parameters", new JSONObject().put("direction",Direction.getRight(drone.getDirection()).toString()));
-                this.drone.setDirection(Direction.getRight(drone.getDirection()));
-            }
-            case ECHO_FRONT -> {
-                step.put("parameters", new JSONObject().put("direction",drone.getDirection().toString()));
-            }
-
-            default->{
-
-            }
-        }
-        //logger.info("** Decision: {}", step.toString());
-        return step;
-    }
-
     /*
     public void sendEcho(String direction){
         this.decision = "echo";
