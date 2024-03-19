d1b1b01d2a16eac14a001cf8519ae1f574949c75
Andrew Habib <andrew9ca@gmail.com>
2024-03-19 19:45:51 -0400

    Refactor code to remove scans while finding the island

--- src/main/java/ca/mcmaster/se2aa4/island/team208/FindIslandSequence.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team208/FindIslandSequence.java
@@ -7,7 +7,6 @@ public class FindIslandSequence implements ActionSequence{
     private List<Result>results;
     private boolean completed;
 
-
     public FindIslandSequence(RadarInterpreter radar, List<Result>results){
         this.radarInterpreter=radar;
         this.results = results;
@@ -15,12 +14,13 @@ public class FindIslandSequence implements ActionSequence{
     }
     @Override
     public void generateNextActions(List<Action> decisionQueue) {
-        decisionQueue.add(Action.SCAN);
         if (this.results.size() - 1 == 0 || decisionQueue.get(this.results.size() - 1) == Action.FLY) {
             decisionQueue.add(Action.ECHO_RIGHT);
         } else if (decisionQueue.get(this.results.size() - 1) == Action.ECHO_RIGHT) {
             if (this.radarInterpreter.getFound().equals("GROUND")) {
                 decisionQueue.add(Action.TURN_RIGHT);
+            } else {
+                decisionQueue.add(Action.FLY);
             }
         } else if (decisionQueue.get(this.results.size() - 1) == Action.TURN_RIGHT) {
             decisionQueue.add(Action.ECHO_FRONT);
@@ -28,10 +28,7 @@ public class FindIslandSequence implements ActionSequence{
             for (int i = 0; i < this.radarInterpreter.getRange(); i++) {
                 decisionQueue.add(Action.FLY);
             }
-            decisionQueue.add(Action.SCAN);
             this.completed = true;
-        } else {
-            decisionQueue.add(Action.FLY);
         }
     }
 
